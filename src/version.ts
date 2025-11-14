c62fd9dbfb8ea1fefdc37b00e7a18512d3321095
René Vogel <rv@eworld.ch>
2025-11-14 16:57:52 +0100

    Update version to 1.1.5

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated version file
 // This gets updated on each build
-export const APP_VERSION = '1.1.4';
-export const BUILD_DATE = '2025-01-14T19:00:00.000Z';
-export const BUILD_TIMESTAMP = 1736880000000;
+export const APP_VERSION = '1.1.5';
+export const BUILD_DATE = '2025-01-14T20:00:00.000Z';
+export const BUILD_TIMESTAMP = 1736883600000;
