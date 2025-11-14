2b3bc74f060463ead43f0470ec3ddd85f6a57b22
René Vogel <rv@eworld.ch>
2025-11-14 16:52:28 +0100

    Update version to 1.1.4

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated version file
 // This gets updated on each build
-export const APP_VERSION = '1.1.3';
-export const BUILD_DATE = '2025-01-14T18:00:00.000Z';
-export const BUILD_TIMESTAMP = 1736876400000;
+export const APP_VERSION = '1.1.4';
+export const BUILD_DATE = '2025-01-14T19:00:00.000Z';
+export const BUILD_TIMESTAMP = 1736880000000;
